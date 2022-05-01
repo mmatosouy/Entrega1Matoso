eb5f976f65b1d31c00ec37a3d572e99a06be17ca
mmatoso <mmatosouy@gmail.com>
2022-05-01 20:03:36 -0300

    4to commit

--- paginasvet/models.py
+++ paginasvet/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class Pedido(models.Model):
+    nombre = models.CharField(max_length=100)
+    email = models.EmailField()
+    descripcion = models.TextField()
