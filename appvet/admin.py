9c4693ea23aee0c6d619a9d8293ce1320ebeb422
mmatoso <mmatosouy@gmail.com>
2022-05-01 20:01:06 -0300

    movi modelos

--- appvet/admin.py
+++ appvet/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Usuario
+from .models import Mascota
+from .models import Pedido
+
+admin.site.register(Usuario)
+admin.site.register(Mascota)
+admin.site.register(Pedido)
 
 # Register your models here.
