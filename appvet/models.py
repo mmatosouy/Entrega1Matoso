9f1c0de19dfee99144a558656bcae897f91319eb
mmatoso <mmatosouy@gmail.com>
2022-05-01 19:30:51 -0300

    clases

--- appvet/models.py
+++ appvet/models.py
@@ -1,3 +1,20 @@
+import email
 from django.db import models
 
-# Create your models here.
+class usuario(models.Model):
+    nombre = models.CharField(max_length=100)
+    apellido = models.CharField(max_length=100)
+    email = models.EmailField()
+    telefono = models.CharField(max_length=20)
+    direccion = models.TextField()
+
+class mascota(models.Model):
+    nombre = models.CharField(max_length=100)
+    raza = models.CharField(max_length=100)
+    nacimiento = models.DateField()
+    descripcion = models.TextField()
+
+class pedido(models.Model):
+    nombre = models.CharField(max_length=100)
+    email = models.EmailField()
+    descripcion = models.TextField()
---
+++ paginasvet/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class PaginasvetConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'paginasvet'
